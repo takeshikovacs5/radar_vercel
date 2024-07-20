69c9a742e0acb85565f81ca54154fe918d326e58
takeshikovacs5 <158284115+takeshikovacs5@users.noreply.github.com>
2024-07-20 12:18:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -129,29 +129,29 @@ def radar_chart():
         takim2 = '-'
 
     def getSeasonId(playerId):
-    headers3 = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'accept-language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
-        'cache-control': 'max-age=0',
-        'if-none-match': '"a9fufto5gfhvxm"',
-        'priority': 'u=0, i',
-        'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'document',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-user': '?1',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
-        }
-
-    response3 = requests.get('https://www.fotmob.com/tr/players/'+str(playerId)+'/', headers=headers3)
-    soup = BeautifulSoup(response3.text, 'html.parser')
-    # 2023/2024 sezonundaki Super Lig seçeneğinin value değerini bul
-    option = soup.find('optgroup', {'label': '2023/2024'}).find('option', string='Super Lig')
-    leagueId = option['value'] if option else "0-0"
-    return leagueId
+        headers3 = {
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7',
+            'cache-control': 'max-age=0',
+            'if-none-match': '"a9fufto5gfhvxm"',
+            'priority': 'u=0, i',
+            'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+            }
+    
+        response3 = requests.get('https://www.fotmob.com/tr/players/'+str(playerId)+'/', headers=headers3)
+        soup = BeautifulSoup(response3.text, 'html.parser')
+        # 2023/2024 sezonundaki Super Lig seçeneğinin value değerini bul
+        option = soup.find('optgroup', {'label': '2023/2024'}).find('option', string='Super Lig')
+        leagueId = option['value'] if option else "0-0"
+        return leagueId
 
     player1_season_id = getSeasonId(player1_fotmob_id)
     player2_season_id = getSeasonId(player2_fotmob_id)
